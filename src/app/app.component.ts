1f62674ec9819e2761627298ad6ccb08b645ffb8
Borromeo <davidpborromeo@gmail.com>
2019-06-13 17:13:40 +0900

    Create environment for nem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {AccountHttp, NEMLibrary, NetworkTypes, Address} from "nem-library";
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'nem-app';
+
+  ngOnInit() {
+    // Initialize NEMLibrary for TEST_NET Network
+    NEMLibrary.bootstrap(NetworkTypes.TEST_NET);
+
+    const address = new Address("TBU64P26SEY3OJ6FEBPEVOZR237ZWY2GJMHMKR7W");
+
+    const accountHttp = new AccountHttp();
+
+    accountHttp.getFromAddress(address).subscribe(accountInfoWithMetaData => {
+        console.log(accountInfoWithMetaData);
+    });
+  }
 }
